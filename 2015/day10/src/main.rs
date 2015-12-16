3bb318e0bed037986eaeb15fbd2d3153a74abf39
Georg Brandl <georg@python.org>
2015-12-16 21:46:03 +0100

    2015: Speedup day 10.

--- 2015/day10/src/main.rs
+++ 2015/day10/src/main.rs
@@ -1,24 +1,31 @@
-use std::fmt::Write;
-
 const INPUT: &'static str = "1321131112";
 
-fn main() {
-    let mut seq = String::from(INPUT);
-    for i in 1..51 {
-        let mut new_seq = String::new();
-        let mut pch = '\n';
-        let mut run = 0;
-        for ch in seq.chars() {
-            if ch != pch && pch != '\n' {
-                write!(new_seq, "{}", run).unwrap();
-                new_seq.push(pch);
-                run = 0;
-            }
-            run += 1;
-            pch = ch;
+fn push_pair(v: &mut Vec<u8>, n: u8, d: u8) {
+    if n >= 10 {
+        for ch in format!("{}", n).chars() {
+            v.push(ch.to_digit(10).unwrap() as u8);
         }
-        write!(new_seq, "{}", run).unwrap();
-        new_seq.push(pch);
+    } else {
+        v.push(n);
+    }
+    v.push(d);
+}
+
+fn main() {
+    let mut seq: Vec<_> = INPUT.chars().map(|ch| ch.to_digit(10).unwrap() as u8).collect();
+    for i in 1..51 {
+        let mut new_seq = Vec::with_capacity(2 * seq.len());
+        let mut dp = seq[0];
+        let mut n = 0;
+        for d in seq {
+            if d != dp {
+                push_pair(&mut new_seq, n, dp);
+                n = 0;
+            }
+            n += 1;
+            dp = d;
+        }
+        push_pair(&mut new_seq, n, dp);
         seq = new_seq;
         if i > 35 && i % 10 == 0 {
             println!("Resulting length after {} iterations: {}", i, seq.len());
