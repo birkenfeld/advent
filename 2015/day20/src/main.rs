8b8ed0e9bf54bc908ef78c0e2d92f5f790cd5d3f
Georg Brandl <georg@python.org>
2015-12-20 20:02:49 +0100

    2015: Day 20.

---
+++ 2015/day20/src/main.rs
@@ -0,0 +1,19 @@
+use std::cmp::min;
+
+const GOAL: u32 = 33100000;
+const N: u32 = GOAL / 10;
+
+fn find(maxhouses: u32, multiplier: u32) -> usize {
+    let mut presents = vec![1u32; N as usize];
+    for elf in 2..N {
+        for house in 1..min(maxhouses, N/elf) {
+            presents[(house * elf) as usize] += multiplier * elf;
+        }
+    }
+    presents.into_iter().position(|p| p >= GOAL).unwrap()
+}
+
+fn main() {
+    println!("Visiting all houses: {}", find(N, 10));
+    println!("Visiting only 50 houses: {}", find(50, 11));
+}
