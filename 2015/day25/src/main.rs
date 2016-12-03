c97cbdd222b332c50ce70c59d53d8d2a0aef8e49
Georg Brandl <georg@python.org>
2016-12-03 22:42:06 +0100

    2015: Day 25.

---
+++ 2015/day25/src/main.rs
@@ -0,0 +1,27 @@
+extern crate advtools;
+
+const ROW: usize = 2981;
+const COL: usize = 3075;
+const FIRST: u64 = 20151125;
+const FACTOR: u64 = 252533;
+const DIVISOR: u64 = 33554393;
+
+fn main() {
+    let mut col = 1;
+    let mut row = 1;
+    let mut code = FIRST;
+    loop {
+        if row == 1 {
+            row = col + 1;
+            col = 1;
+        } else {
+            row -= 1;
+            col += 1;
+        }
+        code = (code * FACTOR) % DIVISOR;
+        if row == ROW && col == COL {
+            println!("Code at ({},{}): {}", ROW, COL, code);
+            break;
+        }
+    }
+}
