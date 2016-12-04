1e16304a46c16721f16af4fbc6ca0e5373d1ac03
Georg Brandl <georg@python.org>
2016-12-04 07:57:04 +0100

    2016: Day 3.

---
+++ 2016/day03/src/main.rs
@@ -0,0 +1,21 @@
+extern crate advtools;
+extern crate itertools;
+
+use itertools::Itertools;
+
+fn possible(s: (u32, u32, u32)) -> usize {
+    (s.0 + s.1 > s.2 && s.1 + s.2 > s.0 && s.2 + s.0 > s.1) as usize
+}
+
+fn main() {
+    let mut num_row = 0;
+    let mut num_col = 0;
+    for (a, b, c) in advtools::iter_input::<(u32, u32, u32)>().tuples() {
+        num_row += possible(a) + possible(b) + possible(c);
+        num_col += possible((a.0, b.0, c.0)) +
+            possible((a.1, b.1, c.1)) +
+            possible((a.2, b.2, c.2));
+    }
+    println!("Possible triangles (by row): {}", num_row);
+    println!("Possible triangles (by column): {}", num_col);
+}
