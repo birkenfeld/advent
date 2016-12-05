a4fdce210094289e379e67978a6edee13982faac
Georg Brandl <g.brandl@fz-juelich.de>
2016-12-05 13:00:12 +0100

    2016: Touch up day 4 a bit.

--- 2016/day04/src/main.rs
+++ 2016/day04/src/main.rs
@@ -1,32 +1,37 @@
 extern crate advtools;
-
-use std::collections::HashMap;
+extern crate fnv;
 
 fn main() {
     let mut np_sector = 0;
     let mut sector_sum = 0;
     for line in advtools::iter_input::<String>() {
+        // parse the input line
         let mut s1 = line.split('[');
         let mut parts = s1.next().unwrap().split('-');
         let checksum = &s1.next().unwrap()[0..5];
         let sector: u32 = parts.next_back().unwrap().parse().unwrap();
-
         let name_parts = parts.collect::<Vec<_>>();
-        let mut counts = HashMap::new();
+
+        // count the digits in the name
+        let mut counts = fnv::FnvHashMap::default();
         for part in &name_parts {
             for ch in part.chars() {
                 *counts.entry(ch).or_insert(0) += 1;
             }
         }
+        // get letters sorted by count, then by alphabet
         let mut sorted_counts = counts.into_iter().map(|(ch, count)| (-count, ch))
                                                   .collect::<Vec<_>>();
         sorted_counts.sort();
-        let should_checksum = sorted_counts.into_iter().take(5)
-                                                       .map(|x| x.1).collect::<String>();
-        if should_checksum == checksum {
+
+        // determine checksum from letter counts
+        let real_checksum = sorted_counts.into_iter().take(5).map(|x| x.1)
+                                                             .collect::<String>();
+        if real_checksum == checksum {
             sector_sum += sector;
 
-            let mut real_name = String::new();
+            // decode real name of room
+            let mut real_name = String::with_capacity(line.len());
             for part in name_parts {
                 for ch in part.chars() {
                     let letter = (ch as u8) - b'a';
@@ -35,7 +40,7 @@ fn main() {
                 }
                 real_name.push(' ');
             }
-            real_name.pop();
+            real_name.pop();  // final space
             if real_name == "northpole object storage" {
                 np_sector = sector;
             }
