7e58cc06a32cffd4a892574a7e3519f441bc3273
Georg Brandl <georg@python.org>
2016-12-05 21:03:55 +0100

    2016: Day 4: use regex, some more touchups.

--- 2016/day04/src/main.rs
+++ 2016/day04/src/main.rs
@@ -1,23 +1,21 @@
 extern crate advtools;
+extern crate regex;
 extern crate fnv;
+
+const TARGET_ROOM: &'static str = "northpole object storage";
+const LINE_FMT: &'static str = r"(.*)-(\d+)\[(.*)\]";
 
 fn main() {
     let mut np_sector = 0;
     let mut sector_sum = 0;
+    let rx = regex::Regex::new(LINE_FMT).unwrap();
     for line in advtools::iter_input::<String>() {
-        // parse the input line
-        let mut s1 = line.split('[');
-        let mut parts = s1.next().unwrap().split('-');
-        let checksum = &s1.next().unwrap()[0..5];
-        let sector: u32 = parts.next_back().unwrap().parse().unwrap();
-        let name_parts = parts.collect::<Vec<_>>();
+        let caps = rx.captures(&line).unwrap();
 
         // count the digits in the name
         let mut counts = fnv::FnvHashMap::default();
-        for part in &name_parts {
-            for ch in part.chars() {
-                *counts.entry(ch).or_insert(0) += 1;
-            }
+        for ch in caps[1].chars().filter(|&ch| ch != '-') {
+            *counts.entry(ch).or_insert(0) += 1;
         }
         // get letters sorted by count, then by alphabet
         let mut sorted_counts = counts.into_iter().map(|(ch, count)| (-count, ch))
@@ -27,21 +25,17 @@ fn main() {
         // determine checksum from letter counts
         let real_checksum = sorted_counts.into_iter().take(5).map(|x| x.1)
                                                              .collect::<String>();
-        if real_checksum == checksum {
+        if real_checksum == caps[3] {
+            let sector = caps[2].parse().unwrap();
             sector_sum += sector;
 
             // decode real name of room
-            let mut real_name = String::with_capacity(line.len());
-            for part in name_parts {
-                for ch in part.chars() {
-                    let letter = (ch as u8) - b'a';
-                    let rotated = (letter + (sector % 26) as u8) % 26;
-                    real_name.push((rotated + b'a') as char);
-                }
-                real_name.push(' ');
-            }
-            real_name.pop();  // final space
-            if real_name == "northpole object storage" {
+            let decode = |ch| if ch == '-' { ' ' } else {
+                let letter = (ch as u8) - b'a';
+                let rotated = (letter + (sector % 26) as u8) % 26;
+                (rotated + b'a') as char
+            };
+            if TARGET_ROOM.chars().eq(caps[1].chars().map(decode)) {
                 np_sector = sector;
             }
         }
