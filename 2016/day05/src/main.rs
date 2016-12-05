5fafe59bd08ac41de85d112b1f6b87cba0d2e87a
Georg Brandl <georg@python.org>
2016-12-05 20:19:54 +0100

    2016: Add comments to day 5.

--- 2016/day05/src/main.rs
+++ 2016/day05/src/main.rs
@@ -24,16 +24,24 @@ fn check(i: usize) -> Option<(usize, u8, u8)> {
 }
 
 fn main() {
+    // the first passcode can be collected directly
     let mut pass_door1 = String::new();
+    // the second passcode needs to be constructed char by char
     let mut pass_door2 = vec![None; LEN];
     let mut n = 0;
+    // we need more batches as long as not all pass_door2 digits are found
+    // (pass_door1 is long done by then)
     while pass_door2.iter().any(|v| v.is_none()) {
+        // get every candidate digit (with 00000 MD5 prefix) in the next batch
         let mut digits = (n..n + BATCH).into_par_iter()
                                        .filter_map(check)
                                        .fold(|| vec![], |mut v, x| { v.push(x); v })
                                        .reduce(|| vec![], |mut v, x| { v.extend(x); v });
         digits.sort();  // by n, then d6, then d7
+        // update passcode for first door, just by order of number
         pass_door1.extend(digits.iter().map(|d| char::from_digit(d.1 as u32, 16).unwrap()));
+        // update passcode for second door, where the index is d6, the code digit
+        // is d7, and the first one wins
         for (_, d6, d7) in digits {
             let d6 = d6 as usize;
             if d6 < LEN && pass_door2[d6].is_none() {
@@ -42,7 +50,8 @@ fn main() {
         }
         n += BATCH;
     }
-    let p2 = pass_door2.into_iter().map(|x| x.unwrap()).collect::<String>();
-    println!("First door: {:?}", &pass_door1[..8]);
-    println!("Second door: {:?}", p2);
+    let pass_door1 = &pass_door1[..8];
+    let pass_door2 = pass_door2.into_iter().map(|x| x.unwrap()).collect::<String>();
+    println!("First door: {}", pass_door1);
+    println!("Second door: {}", pass_door2);
 }
