7a89c366bd2be7b602c97b26959907d62170571d
Georg Brandl <g.brandl@fz-juelich.de>
2016-12-05 12:48:36 +0100

    2016: Refactor day 5: make upper limit dynamic.

--- 2016/day05/src/main.rs
+++ 2016/day05/src/main.rs
@@ -1,12 +1,14 @@
 extern crate crypto;
 extern crate rayon;
 
+use std::char;
 use crypto::md5::Md5;
 use crypto::digest::Digest;
 use rayon::prelude::*;
 
 const INPUT: &'static [u8] = b"uqwqemis";
-const N: usize = 30_000_000;
+const LEN: usize = 8;
+const BATCH: usize = 1_000_000;
 
 fn check(i: usize) -> Option<(usize, u8, u8)> {
     let mut buf = [0u8; 16];
@@ -22,23 +24,25 @@ fn check(i: usize) -> Option<(usize, u8, u8)> {
 }
 
 fn main() {
-    let mut cands1 = (0..N).into_par_iter().filter_map(check)
-                                           .fold(|| Vec::new(), |mut v, x| { v.push(x); v })
-                                           .reduce(|| Vec::new(), |mut v, x| { v.extend(x); v });
-    let mut cands2 = cands1.iter().cloned().map(|(i, d6, d7)| (d6, i, d7)).collect::<Vec<_>>();
-    cands1.sort();
-    cands2.sort();
-    let p1 = cands1.into_iter().take(8).map(|t| format!("{:x}", t.1)).collect::<String>();
-    let mut p2 = vec![16; 8];
-    for (i, _, d) in cands2 {
-        if i >= 8 {
-            break;
+    let mut pass_door1 = String::new();
+    let mut pass_door2 = vec![None; LEN];
+    let mut n = 0;
+    while pass_door2.iter().any(|v| v.is_none()) {
+        let mut digits = (n..n + BATCH).into_par_iter()
+                                       .filter_map(check)
+                                       .fold(|| vec![], |mut v, x| { v.push(x); v })
+                                       .reduce(|| vec![], |mut v, x| { v.extend(x); v });
+        digits.sort();  // by n, then d6, then d7
+        pass_door1.extend(digits.iter().map(|d| char::from_digit(d.1 as u32, 16).unwrap()));
+        for (_, d6, d7) in digits {
+            let d6 = d6 as usize;
+            if d6 < LEN && pass_door2[d6].is_none() {
+                pass_door2[d6] = char::from_digit(d7 as u32, 16);
+            }
         }
-        if p2[i as usize] == 16 {
-            p2[i as usize] = d;
-        }
+        n += BATCH;
     }
-    let p2 = p2.into_iter().map(|x| format!("{:x}", x)).collect::<String>();
-    println!("First door: {:?}", p1);
+    let p2 = pass_door2.into_iter().map(|x| x.unwrap()).collect::<String>();
+    println!("First door: {:?}", &pass_door1[..8]);
     println!("Second door: {:?}", p2);
 }
