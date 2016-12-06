1d3839f62b9a1fcf44ce6cb8dfd0e83ae4737fcc
Georg Brandl <georg@python.org>
2016-12-06 07:44:04 +0100

    2016: Day 6: version without hash map.

--- 2016/day06/src/main.rs
+++ 2016/day06/src/main.rs
@@ -1,21 +1,17 @@
 extern crate advtools;
 
-use std::iter;
-use std::collections::HashMap;
-
 fn main() {
     let mut lines = advtools::iter_input::<String>();
     let first_line = lines.next().unwrap();
-    let mut maps = first_line.chars().map(|ch| iter::once((ch, 1)).collect())
-                                     .collect::<Vec<HashMap<char, i32>>>();
-    for line in lines {
-        for (map, ch) in maps.iter_mut().zip(line.chars()) {
-            *map.entry(ch).or_insert(0) += 1;
+    let mut arrs = vec![[0; 26]; first_line.len()];
+    for line in std::iter::once(first_line).chain(lines) {
+        for (arr, ch) in arrs.iter_mut().zip(line.chars()) {
+            arr[(ch as u8 - b'a') as usize] += 1;
         }
     }
-    let collect_by_freq = |weight| maps.iter().map(|map| {
-        let freqs = advtools::sorted(map.iter().map(|(k, v)| (weight * v, k)));
-        *freqs[0].1
+    let collect_by_freq = |weight| arrs.iter().map(|arr| {
+        let freqs = advtools::sorted(arr.iter().enumerate().map(|(i, v)| (weight * v, i)));
+        (freqs[0].1 as u8 + b'a') as char
     }).collect::<String>();
     println!("Message (most common): {}", collect_by_freq(-1));
     println!("Message (least common): {}", collect_by_freq(1));
