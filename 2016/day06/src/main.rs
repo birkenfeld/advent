68f538ec0e895ed5b4b33c2d19319d35d954ef67
Georg Brandl <georg@python.org>
2016-12-06 07:09:56 +0100

    2016: Day 6.

---
+++ 2016/day06/src/main.rs
@@ -0,0 +1,25 @@
+extern crate advtools;
+
+use std::collections::HashMap;
+
+fn main() {
+    let mut lines = advtools::iter_input::<String>();
+    let mut all_maps = Vec::new();
+    for ch in lines.next().unwrap().chars() {
+        let mut map = HashMap::new();
+        map.insert(ch, 1);
+        all_maps.push(map);
+    }
+    for line in lines {
+        for (map, ch) in all_maps.iter_mut().zip(line.chars()) {
+            *map.entry(ch).or_insert(0) += 1;
+        }
+    }
+    let collect_by_freq = |weight| all_maps.iter().map(|map| {
+        let mut freqs = map.into_iter().map(|(k, v)| (weight * v, k)).collect::<Vec<_>>();
+        freqs.sort();
+        *freqs[0].1
+    }).collect::<String>();
+    println!("Message (most common): {}", collect_by_freq(-1));
+    println!("Message (least common): {}", collect_by_freq(1));
+}
