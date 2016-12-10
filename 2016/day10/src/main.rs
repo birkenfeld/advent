bc33fd796bd6e2e5fa34e981505cc6c0089a0256
Georg Brandl <georg@python.org>
2016-12-10 07:35:33 +0100

    2016: Day 10: make it more idiomatic.

--- 2016/day10/src/main.rs
+++ 2016/day10/src/main.rs
@@ -2,55 +2,56 @@ extern crate advtools;
 
 use std::collections::HashMap;
 
+enum Rule {
+    Out(u32),
+    Bot(u32),
+}
+
+struct Bot {
+    chips: Vec<u32>,
+    rule: (Rule, Rule),
+}
+
 fn main() {
-    let mut rules = HashMap::<u32, (bool, u32, bool, u32)>::new();
-    let mut values = HashMap::<u32, Vec<u32>>::new();
+    let mut bots = HashMap::<u32, Bot>::new();
     let mut outputs = HashMap::<u32, u32>::new();
-    let mut circulating = 0;
+    let mut changes = Vec::new();
 
     for line in advtools::iter_input::<String>() {
         let parts = line.split_whitespace().collect::<Vec<_>>();
         if parts[0] == "value" {
             let val = parts[1].parse().unwrap();
-            let bot = parts[5].parse().unwrap();
-            values.entry(bot).or_insert(vec![]).push(val);
-            circulating += 1;
+            let botno = parts[5].parse().unwrap();
+            changes.push((botno, val));
         } else {
-            let bot = parts[1].parse().unwrap();
-            let lowout = parts[5] == "output";
+            let botno = parts[1].parse().unwrap();
             let low = parts[6].parse().unwrap();
-            let highout = parts[10] == "output";
+            let lowrule = if parts[5] == "output" { Rule::Out(low) } else { Rule::Bot(low) };
             let high = parts[11].parse().unwrap();
-            rules.insert(bot, (lowout, low, highout, high));
+            let highrule = if parts[10] == "output" { Rule::Out(high) } else { Rule::Bot(high) };
+            bots.insert(botno, Bot { chips: vec![], rule: (lowrule, highrule) });
         }
     }
 
-    let mut changes = Vec::new();
-    while circulating > 0 {
-        for (bot, chips) in &mut values {
-            if chips.len() == 2 {
-                let (lowout, low, highout, high) = rules[bot];
-                let mut chips = std::mem::replace(chips, vec![]);
-                chips.sort();
-                if &chips == &[17, 61] {
-                    println!("Comparing 17-61: {}", bot);
+    while !changes.is_empty() {
+        for (botno, chip) in changes.drain(..) {
+            bots.get_mut(&botno).unwrap().chips.push(chip);
+        }
+        for (botno, bot) in &mut bots {
+            if bot.chips.len() == 2 {
+                let chips = advtools::sorted(bot.chips.drain(..));
+                if chips == [17, 61] {
+                    println!("Comparing 17-61: {}", botno);
                 }
-                if lowout {
-                    outputs.insert(low, chips[0]);
-                    circulating -= 1;
-                } else {
-                    changes.push((low, chips[0]));
+                match bot.rule.0 {
+                    Rule::Out(low) => { outputs.insert(low, chips[0]); }
+                    Rule::Bot(low) => { changes.push((low, chips[0])); }
                 }
-                if highout {
-                    outputs.insert(high, chips[1]);
-                    circulating -= 1;
-                } else {
-                    changes.push((high, chips[1]));
+                match bot.rule.1 {
+                    Rule::Out(high) => { outputs.insert(high, chips[1]); }
+                    Rule::Bot(high) => { changes.push((high, chips[1])); }
                 }
             }
-        }
-        for (bot, chip) in changes.drain(..) {
-            values.entry(bot).or_insert(vec![]).push(chip);
         }
     }
 
