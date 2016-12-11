6dfe5b57811627a9f07087e649f9813c6283a2f4
Georg Brandl <georg@python.org>
2016-12-11 19:23:48 +0100

    2016: Day 11: a faster but less intelligible one with single u64 as state.

--- 2016/day11/src/main.rs
+++ 2016/day11/src/main.rs
@@ -1,38 +1,62 @@
-extern crate arrayvec;
-extern crate fnv;
+use std::collections::{VecDeque, HashMap};
 
-use arrayvec::{Array, ArrayVec};
-use std::collections::VecDeque;
-use std::hash::Hash;
+#[derive(Clone, Copy, Hash, PartialEq, Eq)]
+struct State(u64);
 
-const F: u8 = 4;
-
-trait ByteArray: Array<Item=u8> + Clone + Hash + Eq {}
-impl<T: Array<Item=u8> + Clone + Hash + Eq> ByteArray for T {}
-
-#[derive(Debug, Clone, Hash, PartialEq, Eq)]
-struct State<A: ByteArray> {
-    floor:  u8,
-    things: ArrayVec<A>,
-}
-
-impl<A: ByteArray> State<A> {
-    fn is_done(&self) -> bool {
-        self.floor == F-1 && self.things.iter().all(|&floor| floor == F-1)
+impl State {
+    fn new(gens: &[u64], chips: &[u64]) -> Self {
+        assert_eq!(gens.len(), chips.len());
+        let n = gens.len();
+        let mut num = (n as u64) << 58;
+        for (i, &gen) in gens.iter().enumerate() {
+            num |= gen << (i << 2);
+        }
+        for (i, &chip) in chips.iter().enumerate() {
+            num |= chip << ((i + n) << 2);
+        }
+        State(num)
     }
-    fn canonicalize_and_check(&mut self, n: usize) -> bool {
-        // if not ok, no need to do the sorting
-        if !self.things[n..].iter().enumerate().all(
-            |(i, &floor)| self.things[i] == floor ||
-                !self.things[..n].iter().any(|&v| v == floor)) {
+    fn n(&self) -> usize {
+        (self.0 >> 58) as usize
+    }
+    fn thing(&self, i: usize) -> u64 {
+        (self.0 >> (i << 2)) & 0x3
+    }
+    fn set_thing(&mut self, i: usize, f: u64) {
+        self.0 = (self.0 & !(3 << (i << 2))) | (f << (i << 2));
+    }
+    fn floor(&self) -> u64 {
+        self.thing(14)
+    }
+    fn set_floor(&mut self, f: u64) {
+        self.set_thing(14, f);
+    }
+    fn swap_things(&mut self, i: usize, j: usize) {
+        let a = self.thing(i);
+        let b = self.thing(j);
+        self.set_thing(i, b);
+        self.set_thing(j, a);
+    }
+    fn all_above(&self) -> bool {
+        (0..self.n()*2).all(|i| self.thing(i) >= self.floor())
+    }
+    fn is_done(&self) -> bool {
+        self.floor() == 3 && (0..2*self.n()).all(|i| self.thing(i) == 3)
+    }
+    fn canonicalize_and_check(&mut self) -> bool {
+        let n = self.n();
+        // check if state is bad, no need to do the sorting if yes
+        if !(0..n).all(|i| self.thing(i+n) == self.thing(i) ||
+                       !(0..n).any(|j| self.thing(j) == self.thing(i+n))) {
             return false;
         }
+        // sort state - since all gens/chips are equal they are exchangeable
         // unsophisticated O(n^2) insertionsort, does the job
         for i in 1..n {
             let mut j = i;
-            while j > 0 && self.things[j-1] > self.things[j] {
-                self.things.swap(j, j-1);
-                self.things.swap(j+n, j-1+n);
+            while j > 0 && self.thing(j-1) > self.thing(j) {
+                self.swap_things(j, j-1);
+                self.swap_things(j+n, j-1+n);
                 j -= 1;
             }
         }
@@ -40,9 +64,9 @@ impl<A: ByteArray> State<A> {
     }
 }
 
-fn find_steps<A: ByteArray>(n: usize, initial: State<A>) -> Option<usize> {
+fn find_steps(initial: State) -> Option<usize> {
     let mut queue = VecDeque::with_capacity(20000);
-    let mut queued = fnv::FnvHashMap::with_capacity_and_hasher(1000000, Default::default());
+    let mut queued = HashMap::with_capacity(1000000);
     queue.push_back((initial, 0));
     while let Some((state, i)) = queue.pop_front() {
         // check for found solution
@@ -50,31 +74,32 @@ fn find_steps<A: ByteArray>(n: usize, initial: State<A>) -> Option<usize> {
             return Some(i);
         }
         // determine and maybe queue all new states
-        let mut try_state = |mut new_state: State<A>, new_floor, j1, j2| {
-            new_state.floor = new_floor;
-            new_state.things[j1] = new_floor;
-            new_state.things[j2] = new_floor;
-            if new_state.canonicalize_and_check(n) && !queued.contains_key(&new_state) {
-                queued.insert(new_state.clone(), i+1);
+        let mut try_state = |mut new_state: State, new_floor, j1, j2| {
+            new_state.set_floor(new_floor);
+            new_state.set_thing(j1, new_floor);
+            new_state.set_thing(j2, new_floor);
+            if new_state.canonicalize_and_check() && !queued.contains_key(&new_state) {
+                queued.insert(new_state, i+1);
                 queue.push_back((new_state, i+1));
             }
         };
         for new_floor in 0..4 {
             // only move to adjacent floors
-            if !(new_floor + 1 == state.floor || new_floor == state.floor + 1) {
+            if !(new_floor + 1 == state.floor() || new_floor == state.floor() + 1) {
                 continue;
             }
             // don't move down if all floors below are empty
-            if new_floor < state.floor && state.things.iter().all(|&v| v >= state.floor) {
+            if new_floor < state.floor() && state.all_above() {
                 continue;
             }
-            for j1 in 0..2*n {
-                if state.things[j1] == state.floor {
-                    try_state(state.clone(), new_floor, j1, j1);
-
+            for j1 in 0..2*state.n() {
+                if state.thing(j1) == state.floor() {
+                    // one-thing moves
+                    try_state(state, new_floor, j1, j1);
+                    // two-thing moves
                     for j2 in 0..j1 {
-                        if state.things[j2] == state.floor {
-                            try_state(state.clone(), new_floor, j1, j2);
+                        if state.thing(j2) == state.floor() {
+                            try_state(state, new_floor, j1, j2);
                         }
                     }
                 }
@@ -85,8 +110,8 @@ fn find_steps<A: ByteArray>(n: usize, initial: State<A>) -> Option<usize> {
 }
 
 fn main() {
-    let state1 = State { floor:  0, things: [0, 0, 0, 0, 0, 1, 1, 0, 0, 0].into() };
-    println!("Min. # steps (5 chips): {:?}", find_steps(5, state1).unwrap());
-    let state2 = State { floor:  0, things: [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0].into() };
-    println!("Min. # steps (7 chips): {:?}", find_steps(7, state2).unwrap());
+    let state1 = State::new(&[0, 0, 0, 0, 0], &[1, 1, 0, 0, 0]);
+    println!("Min. # steps (5 chips): {:?}", find_steps(state1).unwrap());
+    let state2 = State::new(&[0, 0, 0, 0, 0, 0, 0], &[1, 1, 0, 0, 0, 0, 0]);
+    println!("Min. # steps (7 chips): {:?}", find_steps(state2).unwrap());
 }
