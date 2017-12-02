015ac55ee63b6d6278331883f67c27e7945e6fc5
Georg Brandl <georg@python.org>
2017-12-02 10:06:22 +0100

    2017: Day 2: prettify a bit.

--- 2017/src/bin/day02.rs
+++ 2017/src/bin/day02.rs
@@ -6,15 +6,14 @@ use itertools::Itertools;
 fn main() {
     let input = advtools::iter_input::<Vec<i32>>().collect_vec();
     let cksum1 = input.iter().map(|cols| cols.iter().minmax().into_option().unwrap())
-                             .map(|(&min, &max)| max - min)
+                             .map(|(min, max)| max - min)
                              .sum::<i32>();
-    let div = |a: i32, b: i32| {
-        if a % b == 0 { Some(a / b) }
-        else if b % a == 0 { Some(b / a) }
-        else { None }
-    };
     let cksum2 = input.iter().map(|cols| {
-        cols.iter().tuple_combinations().filter_map(|(&a, &b)| div(a, b)).next().unwrap()
+        cols.iter().tuple_combinations().filter_map(|(a, b)| {
+            if a % b == 0 { Some(a / b) }
+            else if b % a == 0 { Some(b / a) }
+            else { None }
+        }).next().unwrap()
     }).sum::<i32>();
     println!("Checksum 1: {}", cksum1);
     println!("Checksum 2: {}", cksum2);
