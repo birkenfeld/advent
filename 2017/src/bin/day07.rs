7f618be8cbb1151c9f2045e99bbe49e65fa3d89e
Georg Brandl <georg@python.org>
2017-12-07 17:50:08 +0100

    2017: Day 7: rewrite using petgraph.

--- 2017/src/bin/day07.rs
+++ 2017/src/bin/day07.rs
@@ -1,79 +1,46 @@
 extern crate advtools;
+extern crate petgraph;
 
 use std::collections::HashMap;
-use std::mem;
-
-struct Tree<'a> {
-    childmap: HashMap<&'a str, Vec<&'a str>>,
-    weights: HashMap<&'a str, i32>,
-    totals: HashMap<&'a str, i32>,
-}
-
-impl<'a> Tree<'a> {
-    fn child_weight(&self, n: &str) -> i32 {
-        self.childmap[n].iter().map(|v| self.weight(v)).sum::<i32>()
-    }
-
-    fn weight(&self, n: &str) -> i32 {
-        self.weights[n] + self.child_weight(n)
-    }
-
-    fn find_root(&'a self, node: &'a str) -> &'a str {
-        for (parent, children) in &self.childmap {
-            if children.iter().any(|v| *v == node) {
-                return self.find_root(parent);
-            }
-        }
-        node
-    }
-
-    fn check_children(&self, node: &str) -> Option<i32> {
-        if self.childmap[node].is_empty() { return None; }
-        let mut odd = None;
-        let mut last = self.totals[self.childmap[node][0]];
-        for (child, i) in self.childmap[node][1..].iter().zip(1..) {
-            let el = self.totals[child];
-            if el != last {
-                if odd.is_none() {
-                    odd = Some(((el, i), (last, i-1)));
-                }
-            } else if let Some((ref mut odd, ref mut reg)) = odd {
-                if odd.0 != el {
-                    mem::swap(odd, reg);
-                }
-            }
-            last = el;
-        }
-        odd.map(|((_, odd_idx), (normal, _))| normal - self.child_weight(self.childmap[node][odd_idx]))
-    }
-
-    fn find_required_weight(&self, root: &str) -> Option<i32> {
-        self.childmap[root].iter()
-                           .flat_map(|ch| self.find_required_weight(ch))
-                           .next()
-                           .or_else(|| self.check_children(root))
-    }
-}
+use std::collections::hash_map::Entry;
+use petgraph::prelude::*;
 
 fn main() {
     let input = advtools::iter_input::<String>().collect::<Vec<_>>();
-    let mut childmap = HashMap::new();
-    let mut weights = HashMap::new();
+    let mut name2ix = HashMap::new();
+    let mut graph = Graph::new();
     for line in &input {
         let mut split = line.split_whitespace();
         let name = split.next().unwrap();
         let weight = split.next().unwrap().trim_matches(&['(', ')'][..]).parse::<i32>().unwrap();
-        let children = split.skip(1).map(|v| v.trim_matches(',')).collect::<Vec<_>>();
-        weights.insert(name, weight);
-        childmap.insert(name, children);
+        let ix = match name2ix.entry(name) {
+            Entry::Occupied(e) => { graph[*e.get()] = weight; *e.get() },
+            Entry::Vacant(e) =>   { let ix = graph.add_node(weight); *e.insert(ix) },
+        };
+        for childname in split.skip(1).map(|v| v.trim_matches(',')) {
+            let cix = *name2ix.entry(childname).or_insert_with(|| graph.add_node(0));
+            graph.add_edge(ix, cix, 0i32);
+        }
     }
-    let mut tree = Tree { childmap, weights, totals: HashMap::new() };
-    let mut totals = HashMap::new();
-    for &node in tree.childmap.keys() {
-        totals.insert(node, tree.weight(node));
+    let root = graph.externals(Incoming).next().unwrap();
+    println!("Bottom program: {}", name2ix.iter().find(|e| e.1 == &root).unwrap().0);
+
+    let mut dfs = DfsPostOrder::new(&graph, root);
+    let mut req_weight = 0;
+    while let Some(src_ix) = dfs.next(&graph) {
+        let mut weights = HashMap::new();
+        let mut walker = graph.neighbors(src_ix).detach();
+        while let Some((edge_ix, tgt_ix)) = walker.next(&graph) {
+            let edge_wt = graph[tgt_ix] + graph.edges(tgt_ix).map(|e| e.weight()).sum::<i32>();
+            *weights.entry(edge_wt).or_insert(0) += 1;
+            graph[edge_ix] = edge_wt;
+        }
+        if let Some((offending_weight, _)) = weights.into_iter().find(|&(_, c)| c == 1) {
+            let (offending, normal): (Vec<_>, Vec<_>) =
+                graph.edges(src_ix).partition(|ex| *ex.weight() == offending_weight);
+            req_weight = graph[normal[0].id()] - graph[offending[0].id()] + graph[offending[0].target()];
+            break;
+        }
     }
-    tree.totals = totals;
-    let root = tree.find_root(tree.weights.keys().next().unwrap());
-    println!("Bottom program: {}", root);
-    println!("Required weight: {}", tree.find_required_weight(root).unwrap());
+    println!("Required weight: {}", req_weight);
 }
