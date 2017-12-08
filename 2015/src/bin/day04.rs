f5dee2b413a1e1eb08ff0304e2f928d69cad5374
Georg Brandl <g.brandl@fz-juelich.de>
2017-12-08 07:59:41 +0100

    2015: Day 4: use rayon par_iter.

--- 2015/src/bin/day04.rs
+++ 2015/src/bin/day04.rs
@@ -2,53 +2,35 @@ extern crate md5;
 extern crate rayon;
 extern crate itoa;
 
-use std::cmp::min;
 use std::sync::mpsc;
 use md5::{Digest, Md5};
+use rayon::prelude::*;
 
-const INPUT: &'static [u8] = b"yzbqklnj";
-const N: usize = 10_000_000;
+const INPUT: &[u8] = b"yzbqklnj";
+const N: u64 = 10_000_000;
 
-fn check(i: usize, tx: &mut mpsc::Sender<(usize, usize)>) {
+fn check(i: u64, tx: &mpsc::SyncSender<(u64, bool)>) {
     let mut ibuf = [0u8; 16];
     let mut hash = Md5::new();
     let n = itoa::write(&mut ibuf[..], i).unwrap();
     hash.input(INPUT);
     hash.input(&ibuf[..n]);
     let buf = hash.hash();
-    if buf[0] | buf[1] == 0 {
-        if buf[2] & 0xF0 == 0 {
-            tx.send((5, i)).unwrap();
-            if buf[2] == 0 {
-                tx.send((6, i)).unwrap();
-            }
-        }
-    }
-}
-
-fn check_parallel(from: usize, len: usize, mut tx: mpsc::Sender<(usize, usize)>) {
-    if len <= 1000 {
-        for v in from..from+len {
-            check(v, &mut tx);
-        }
-    } else {
-        let half = len / 2;
-        let txc = tx.clone();
-        rayon::join(|| check_parallel(from, half, txc),
-                    || check_parallel(from + half, len - half, tx));
+    if (buf[0] | buf[1] == 0) && (buf[2] & 0xF0 == 0) {
+        tx.send((i, buf[2] == 0)).unwrap();
     }
 }
 
 fn main() {
-    let (tx, rx) = mpsc::channel();
-    check_parallel(0, N, tx);
-    let mut min5z = 0;
-    let mut min6z = 0;
-    while let Ok((nzeros, number)) = rx.recv() {
-        if nzeros == 5 {
-            min5z = if min5z == 0 { number } else { min(min5z, number) };
-        } else if nzeros == 6 {
-            min6z = if min6z == 0 { number } else { min(min6z, number) };
+    let (tx, rx) = mpsc::sync_channel(256);
+    (0..N).into_par_iter().for_each(|n| check(n, &tx));
+    drop(tx);
+    let mut min5z = u64::max_value();
+    let mut min6z = u64::max_value();
+    while let Ok((number, sixzeros)) = rx.recv() {
+        min5z = min5z.min(number);
+        if sixzeros {
+            min6z = min6z.min(number);
         }
     }
     println!("First 5-zero hash for: {}", min5z);
