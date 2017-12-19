9f891f8925e57c92a75776907503c7f49877ace3
Georg Brandl <georg@python.org>
2017-12-19 19:52:40 +0100

    2017: Day 15: rewrite using itertools::iterate.

--- 2017/src/bin/day15.rs
+++ 2017/src/bin/day15.rs
@@ -1,25 +1,24 @@
+extern crate advtools;
+use advtools::prelude::*;
+
 const START_A:  u64 = 699;
 const FACTOR_A: u64 = 16807;
 const START_B:  u64 = 124;
 const FACTOR_B: u64 = 48271;
 
-fn mult_and_check<F: Fn(u64) -> bool>(a: &mut u64, b: u64, accept: F) -> bool {
+fn mult_mod(a: &u64, b: u64) -> u64 {
     // http://www.firstpr.com.au/dsp/rand31/#Simpler
-    let prod = *a * b;
+    let prod = a * b;
     let res = (prod >> 31) + (prod & 0x7FFF_FFFF);
-    *a = (res & 0x7FFF_FFFF) + (res >> 31);
-    accept(*a)
+    (res & 0x7FFF_FFFF) + (res >> 31)
 }
 
-fn compare_seqs<FA, FB>(n: u32, accept_a: FA, accept_b: FB) -> usize
-    where FA: Fn(u64) -> bool, FB: Fn(u64) -> bool
+fn compare_seqs<FA, FB>(n: usize, accept_a: FA, accept_b: FB) -> usize
+    where FA: Fn(&u64) -> bool, FB: Fn(&u64) -> bool
 {
-    let (mut a, mut b) = (START_A, START_B);
-    (0..n).filter(|_| {
-        while !mult_and_check(&mut a, FACTOR_A, &accept_a) { }
-        while !mult_and_check(&mut b, FACTOR_B, &accept_b) { }
-        a & 0xFFFF == b & 0xFFFF
-    }).count()
+    let ia = itertools::iterate(START_A, |a| mult_mod(a, FACTOR_A)).filter(accept_a);
+    let ib = itertools::iterate(START_B, |b| mult_mod(b, FACTOR_B)).filter(accept_b);
+    ia.zip(ib).take(n).filter(|&(a, b)| a as u16 == b as u16).count()
 }
 
 fn main() {
