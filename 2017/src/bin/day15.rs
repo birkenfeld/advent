6d0c6b8b65dd72d084a9d0e9afe9edc64b5985b7
Georg Brandl <georg@python.org>
2017-12-19 19:20:55 +0100

    2017: Day 15: optimize modulo operation.

--- 2017/src/bin/day15.rs
+++ 2017/src/bin/day15.rs
@@ -2,27 +2,27 @@ const START_A:  u64 = 699;
 const FACTOR_A: u64 = 16807;
 const START_B:  u64 = 124;
 const FACTOR_B: u64 = 48271;
-const MODULUS:  u64 = (1 << 31) - 1;
 
-fn compare_seqs<FA, FB>(n: u64, accept_a: FA, accept_b: FB) -> usize
+fn mult_and_check<F: Fn(u64) -> bool>(a: &mut u64, b: u64, accept: F) -> bool {
+    // http://www.firstpr.com.au/dsp/rand31/#Simpler
+    let prod = *a * b;
+    let res = (prod >> 31) + (prod & 0x7FFF_FFFF);
+    *a = (res & 0x7FFF_FFFF) + (res >> 31);
+    accept(*a)
+}
+
+fn compare_seqs<FA, FB>(n: u32, accept_a: FA, accept_b: FB) -> usize
     where FA: Fn(u64) -> bool, FB: Fn(u64) -> bool
 {
-    (0..n).scan((START_A, START_B), |vals, _| {
-        loop {
-            vals.0 = (vals.0 * FACTOR_A) % MODULUS;
-            if accept_a(vals.0) { break; }
-        }
-        loop {
-            vals.1 = (vals.1 * FACTOR_B) % MODULUS;
-            if accept_b(vals.1) { break; }
-        }
-        Some(vals.0 & 0xFFFF == vals.1 & 0xFFFF)
-    }).filter(|&b| b).count()
+    let (mut a, mut b) = (START_A, START_B);
+    (0..n).filter(|_| {
+        while !mult_and_check(&mut a, FACTOR_A, &accept_a) { }
+        while !mult_and_check(&mut b, FACTOR_B, &accept_b) { }
+        a & 0xFFFF == b & 0xFFFF
+    }).count()
 }
 
 fn main() {
     println!("Accepted #1: {}", compare_seqs(40_000_000, |_| true, |_| true));
-    println!("Accepted #2: {}", compare_seqs(5_000_000,
-                                             |va| va & 3 == 0,
-                                             |vb| vb & 7 == 0));
+    println!("Accepted #2: {}", compare_seqs(5_000_000, |a| a & 3 == 0, |b| b & 7 == 0));
 }
