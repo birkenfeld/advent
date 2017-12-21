893b328c17939347411a78969ac03d40bcdfcdef
Georg Brandl <g.brandl@fz-juelich.de>
2017-12-21 17:52:37 +0100

    2017: Day 21: optimize and clarify slightly.

--- 2017/src/bin/day21.rs
+++ 2017/src/bin/day21.rs
@@ -1,5 +1,32 @@
 extern crate advtools;
+extern crate rayon;
+
 use advtools::prelude::*;
+use rayon::prelude::*;
+
+type S2 = (u8,u8,u8,u8);
+type S3 = (u8,u8,u8,u8,u8,u8,u8,u8,u8);
+
+enum State {
+    One(Vec<Vec<S3>>),
+    Two(Vec<Vec<S2>>),
+    Three(Vec<Vec<S2>>),
+}
+
+impl State {
+    fn sum(&self) -> u32 {
+        match *self {
+            State::One(ref v) =>
+                v.iter().map(|w| w.iter().map(
+                    |i| (i.0 + i.1 + i.2 + i.3 + i.4 + i.5 + i.6 + i.7 + i.8) as u32
+                ).sum::<u32>()).sum(),
+            State::Two(ref v) | State::Three(ref v) =>
+                v.iter().map(|w| w.iter().map(
+                    |i| (i.0 + i.1 + i.2 + i.3) as u32
+                ).sum::<u32>()).sum(),
+        }
+    }
+}
 
 fn pixel(ch: char) -> u8 {
     match ch {
@@ -9,125 +36,122 @@ fn pixel(ch: char) -> u8 {
     }
 }
 
-fn f2x2(mut v: Vec<u8>) -> Vec<u8> {
-    v.swap(0, 1);
-    v.swap(2, 3);
-    v
+fn square2x2(v: Vec<u8>) -> S2 {
+    (v[0], v[1], v[2], v[3])
 }
 
-fn r2x2(mut v: Vec<u8>) -> Vec<u8> {
-    v.swap(0, 1);
-    v.swap(2, 3);
-    v.swap(0, 3);
-    v
+fn square3x3(v: Vec<u8>) -> S3 {
+    (v[0], v[1], v[2], v[3], v[4], v[5], v[6], v[7], v[8])
 }
 
-fn f3x3(mut v: Vec<u8>) -> Vec<u8> {
-    v.swap(0, 2);
-    v.swap(3, 5);
-    v.swap(6, 8);
-    v
+fn flip2x2(v: S2) -> S2 {
+    (v.1, v.0,
+     v.3, v.2)
 }
 
-fn r3x3(mut v: Vec<u8>) -> Vec<u8> {
-    v.swap(0, 2);
-    v.swap(1, 5);
-    v.swap(0, 8);
-    v.swap(1, 3);
-    v.swap(3, 7);
-    v.swap(0, 6);
-    v
+fn rotate2x2(v: S2) -> S2 {
+    (v.2, v.0,
+     v.3, v.1)
 }
 
-fn advance(step: u32, state: Vec<Vec<u8>>, repls: &HashMap<Vec<u8>, Vec<u8>>) -> Vec<Vec<u8>> {
-    match step % 3 {
-        0 => {
-            let mut new = vec![vec![]; state.len()*2];
-            for j in (0..state[0].len()).step(9) {
-                for (i, row) in state.iter().enumerate() {
-                    let repl = &repls[&row[j..j+9]];
-                    new[2*i  ].extend(vec![repl[0], repl[1], repl[4], repl[5],
-                                           repl[2], repl[3], repl[6], repl[7]]);
-                    new[2*i+1].extend(vec![repl[8], repl[9], repl[12], repl[13],
-                                           repl[10], repl[11], repl[14], repl[15]]);
+fn flip3x3(v: S3) -> S3 {
+    (v.2, v.1, v.0,
+     v.5, v.4, v.3,
+     v.8, v.7, v.6)
+}
+
+fn rotate3x3(v: S3) -> S3 {
+    (v.6, v.3, v.0,
+     v.7, v.4, v.1,
+     v.8, v.5, v.2)
+}
+
+fn advance(state: State, repls2x2: &HashMap<S2, S3>, repls3x3: &HashMap<S3, (S2,S2,S2,S2)>) -> State {
+    match state {
+        State::One(state) => {
+            State::Two(state.into_par_iter().flat_map(|row| {
+                let mut new1 = Vec::new();
+                let mut new2 = Vec::new();
+                for square in row {
+                    let (repl1, repl2, repl3, repl4) = repls3x3[&square];
+                    new1.push(repl1);
+                    new1.push(repl2);
+                    new2.push(repl3);
+                    new2.push(repl4);
                 }
-            }
-            new
+                vec![new1, new2]
+            }).collect())
         }
-        1 => {
-            let mut new = vec![vec![]; state.len()*3/2];
-            for j in (0..state[0].len()).step(8) {
-                for (i, (row1, row2)) in state.iter().tuples().enumerate() {
-                    let repl1 = &repls[&row1[j..j+4]];
-                    let repl2 = &repls[&row1[j+4..j+8]];
-                    let repl3 = &repls[&row2[j..j+4]];
-                    let repl4 = &repls[&row2[j+4..j+8]];
-                    new[3*i  ].extend(vec![repl1[0], repl1[1], repl1[3], repl1[4],
-                                           repl1[2], repl2[0], repl1[5], repl2[3],
-                                           repl2[1], repl2[2], repl2[4], repl2[5]]);
-                    new[3*i+1].extend(vec![repl1[6], repl1[7], repl3[0], repl3[1],
-                                           repl1[8], repl2[6], repl3[2], repl4[0],
-                                           repl2[7], repl2[8], repl4[1], repl4[2]]);
-                    new[3*i+2].extend(vec![repl3[3], repl3[4], repl3[6], repl3[7],
-                                           repl3[5], repl4[3], repl3[8], repl4[6],
-                                           repl4[4], repl4[5], repl4[7], repl4[8]]);
+        State::Two(state) => {
+            State::Three(state.par_chunks(2).flat_map(|rows| {
+                let mut new1 = Vec::new();
+                let mut new2 = Vec::new();
+                let mut new3 = Vec::new();
+                for ((sq1, sq2), (sq3, sq4)) in rows[0].iter().tuples().zip(rows[1].iter().tuples()) {
+                    let repl1 = &repls2x2[&sq1];
+                    let repl2 = &repls2x2[&sq2];
+                    let repl3 = &repls2x2[&sq3];
+                    let repl4 = &repls2x2[&sq4];
+                    new1.push((repl1.0, repl1.1, repl1.3, repl1.4));
+                    new1.push((repl1.2, repl2.0, repl1.5, repl2.3));
+                    new1.push((repl2.1, repl2.2, repl2.4, repl2.5));
+                    new2.push((repl1.6, repl1.7, repl3.0, repl3.1));
+                    new2.push((repl1.8, repl2.6, repl3.2, repl4.0));
+                    new2.push((repl2.7, repl2.8, repl4.1, repl4.2));
+                    new3.push((repl3.3, repl3.4, repl3.6, repl3.7));
+                    new3.push((repl3.5, repl4.3, repl3.8, repl4.6));
+                    new3.push((repl4.4, repl4.5, repl4.7, repl4.8));
                 }
-            }
-            new
+                vec![new1, new2, new3]
+            }).collect())
         }
-        2 => {
-            let mut new = vec![vec![]; state.len()];
-            for j in (0..state[0].len()).step(4) {
-                for (i, row) in state.iter().enumerate() {
-                    new[i].extend(repls[&row[j..j+4]].clone());
-                }
-            }
-            new
+        State::Three(state) => {
+            State::One(state.into_par_iter().map(|row| {
+                row.into_iter().map(|square| repls2x2[&square]).collect()
+            }).collect())
         }
-        _ => unreachable!()
+    }
+}
+
+fn prepare<P, Q>(mut pat: P, repl: Q, flip: fn(P) -> P, rot: fn(P) -> P, map: &mut HashMap<P, Q>)
+    where P: std::hash::Hash + Eq + Copy, Q: Copy
+{
+    for _ in 0..4 {
+        map.insert(pat, repl);
+        map.insert(flip(pat), repl);
+        pat = rot(pat);
     }
 }
 
 fn main() {
-    let mut state: Vec<Vec<u8>> = vec![vec![0, 1, 0,
-                                            0, 0, 1,
-                                            1, 1, 1]];
-    let mut repls = HashMap::new();
+    let mut repls2x2 = HashMap::new();
+    let mut repls3x3 = HashMap::new();
     for line in iter_input::<Vec<String>>() {
-        let pattern = line[0].chars().filter(|&c| c != '/').map(pixel).collect_vec();
+        let rpat = line[0].chars().filter(|&c| c != '/').map(pixel).collect_vec();
         let repl = line[2].chars().filter(|&c| c != '/').map(pixel).collect_vec();
         if line[0].len() == 5 {
-            let flipped = f2x2(pattern.clone());
-            repls.insert(r2x2(flipped.clone()), repl.clone());
-            repls.insert(r2x2(r2x2(flipped.clone())), repl.clone());
-            repls.insert(r2x2(r2x2(r2x2(flipped.clone()))), repl.clone());
-            repls.insert(flipped, repl.clone());
-            repls.insert(r2x2(pattern.clone()), repl.clone());
-            repls.insert(r2x2(r2x2(pattern.clone())), repl.clone());
-            repls.insert(r2x2(r2x2(r2x2(pattern.clone()))), repl.clone());
-            repls.insert(pattern, repl);
+            prepare(square2x2(rpat), square3x3(repl), flip2x2, rotate2x2, &mut repls2x2);
         } else {
-            let flipped = f3x3(pattern.clone());
-            repls.insert(r3x3(flipped.clone()), repl.clone());
-            repls.insert(r3x3(r3x3(flipped.clone())), repl.clone());
-            repls.insert(r3x3(r3x3(r3x3(flipped.clone()))), repl.clone());
-            repls.insert(flipped, repl.clone());
-            repls.insert(r3x3(pattern.clone()), repl.clone());
-            repls.insert(r3x3(r3x3(pattern.clone())), repl.clone());
-            repls.insert(r3x3(r3x3(r3x3(pattern.clone()))), repl.clone());
-            repls.insert(pattern, repl);
+            let repls = (
+                (repl[0],  repl[1],  repl[4],  repl[5]),
+                (repl[2],  repl[3],  repl[6],  repl[7]),
+                (repl[8],  repl[9],  repl[12], repl[13]),
+                (repl[10], repl[11], repl[14], repl[15]),
+            );
+            prepare(square3x3(rpat), repls, flip3x3, rotate3x3, &mut repls3x3);
         }
     }
 
-    for n in 0..5 {
-        state = advance(n, state, &repls);
+    let mut state = State::One(vec![vec![(0, 1, 0,
+                                          0, 0, 1,
+                                          1, 1, 1)]]);
+    for _ in 0..5 {
+        state = advance(state, &repls2x2, &repls3x3);
     }
-    println!("Lights on after 5: {}", state.iter().map(
-        |row| row.iter().map(|&v| v as u32).sum::<u32>()).sum::<u32>());
+    println!("Lights on after 5: {}", state.sum());
 
-    for n in 5..18 {
-        state = advance(n, state, &repls);
+    for _ in 5..18 {
+        state = advance(state, &repls2x2, &repls3x3);
     }
-    println!("Lights on after 18: {}", state.iter().map(
-        |row| row.iter().map(|&v| v as u32).sum::<u32>()).sum::<u32>());
+    println!("Lights on after 18: {}", state.sum());
 }
