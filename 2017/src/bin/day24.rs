e1aefd53b9ceffb4412ec9e67f6c6e74ee1e9e58
Georg Brandl <georg@python.org>
2017-12-24 09:33:52 +0100

    2017: Day 24: make it a bit nicer using Cell.

--- 2017/src/bin/day24.rs
+++ 2017/src/bin/day24.rs
@@ -1,13 +1,14 @@
 extern crate advtools;
 use advtools::prelude::*;
+use std::cell::Cell;
 
-fn find_bridges(parts: &[(u32, u32)], used: &mut [bool], connect: u32, mut strength: u32,
-                mut length: u32, strongest: &mut u32, longest: &mut (u32, u32)) {
-    for (i, &(conn_a, conn_b)) in parts.iter().enumerate() {
-        if (conn_a == connect || conn_b == connect) && !used[i] {
+fn find_bridges(parts: &[(u32, u32, Cell<bool>)], connect: u32, mut strength: u32, mut length: u32,
+                strongest: &mut u32, longest: &mut (u32, u32)) {
+    for &(conn_a, conn_b, ref used) in parts {
+        if (conn_a == connect || conn_b == connect) && !used.get() {
             let new_connect = if conn_a == connect { conn_b } else { conn_a };
             // Mark part as used and update our state.
-            used[i] = true;
+            used.set(true);
             length += 1;
             strength += conn_a + conn_b;
             // Check if we have a new strength/length record.
@@ -15,25 +16,25 @@ fn find_bridges(parts: &[(u32, u32)], used: &mut [bool], connect: u32, mut stren
             // (compare length first, then strength).
             *strongest = strength.max(*strongest);
             *longest = (length, strength).max(*longest);
-            find_bridges(parts, used, new_connect, strength, length, strongest, longest);
+            find_bridges(parts, new_connect, strength, length, strongest, longest);
             // Restore previous state for trying the next part.
             strength -= conn_a + conn_b;
             length -= 1;
-            used[i] = false;
+            used.set(false);
         }
     }
 }
 
 fn main() {
-    let parts = iter_input::<String>().map(
-        |line| line.split('/').map(to_u32).collect_tuple().unwrap()
-    ).collect_vec();
+    let parts = iter_input::<String>().map(|line| {
+        let mut conns = line.split('/').map(to_u32);
+        (conns.item(), conns.item(), Cell::default())
+    }).collect_vec();
 
-    let mut used = vec![false; parts.len()];
     let mut strongest = 0;
     let mut longest = (0, 0);
     // Go through all bridge combinations recursively, using DFS.
-    find_bridges(&parts, &mut used, 0, 0, 0, &mut strongest, &mut longest);
+    find_bridges(&parts, 0, 0, 0, &mut strongest, &mut longest);
     // Part 1: Find maximum strength of any bridge.
     println!("Max bridge strength: {}", strongest);
     // Part 2: Find maximum strength of longest bridges.
