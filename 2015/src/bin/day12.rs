53130f2f1098676bbce62d26fec664ba87033247
Georg Brandl <georg@python.org>
2018-12-03 06:05:11 +0100

    2015: Simplify JSON handling.

--- 2015/src/bin/day12.rs
+++ 2015/src/bin/day12.rs
@@ -5,20 +5,20 @@ use advtools::input::input_file;
 use serde_json::{Value, from_reader};
 use serde_json::Value::*;
 
-fn sum_up(val: Value, no_reds: bool) -> i64 {
+fn sum_up(val: &Value, no_reds: bool) -> i64 {
     match val {
         Number(v) => v.as_i64().unwrap(),
         String(_) | Bool(_) | Null => 0,
-        Array(vs) => vs.into_iter().map(|v| sum_up(v, no_reds)).sum(),
-        Object(map) => map.into_iter().fold((false, 0), |acc, kv| {
-            let has_red = acc.0 || (no_reds && kv.1 == String("red".into()));
+        Array(vs) => vs.iter().map(|v| sum_up(v, no_reds)).sum(),
+        Object(map) => map.iter().fold((false, 0), |acc, kv| {
+            let has_red = acc.0 || (no_reds && kv.1 == "red");
             (has_red, if has_red { 0 } else { acc.1 + sum_up(kv.1, no_reds) })
         }).1,
     }
 }
 
 fn main() {
-    let doc: Value = from_reader(input_file()).unwrap();
-    println!("Sum with reds: {:?}", sum_up(doc.clone(), false));
-    println!("Sum without reds: {:?}", sum_up(doc, true));
+    let doc: Value = from_reader(input_file()).expect("input not valid JSON");
+    println!("Sum with reds: {:?}", sum_up(&doc, false));
+    println!("Sum without reds: {:?}", sum_up(&doc, true));
 }
