12a0c14bce775d202e3d95106be1ca748de0751e
Georg Brandl <georg@python.org>
2019-12-19 11:49:18 +0100

    2019: Remove unneded libraries.

--- 2019/src/bin/day07.rs
+++ 2019/src/bin/day07.rs
@@ -6,7 +6,7 @@ fn main() {
     let code = Machine::parse(&input_string());
 
     // Find the maximum signal by testing all permutations of 0..5.
-    let max_signal = permutohedron::Heap::new(&mut [0, 1, 2, 3, 4]).map(|phases| {
+    let max_signal = (0..5).permutations(5).map(|phases| {
         // This fold applies the signal to all five machines in order.
         phases.iter().fold(0.into(), |signal, &phase| {
             Machine::new(&code).run(vec![phase.into(), signal]).unwrap()
@@ -17,7 +17,7 @@ fn main() {
 
     // Same spiel as in part 1, but a bit more complex since we have
     // to loop until a machine halts.
-    let max_signal = permutohedron::Heap::new(&mut [5, 6, 7, 8, 9]).map(|phases| {
+    let max_signal = (5..10).permutations(5).map(|phases| {
         let mut signal: Int = 0.into();
         let mut machines =
             phases.iter().map(|&ph| Machine::new(&code).with_input(Some(ph))).collect_vec();
