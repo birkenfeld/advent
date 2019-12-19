3cf8832d0013150061fa04e101e6f52afc346521
Georg Brandl <georg@python.org>
2019-12-19 11:49:12 +0100

    2019: Speed up day 4 with rayon.

--- 2019/src/bin/day04.rs
+++ 2019/src/bin/day04.rs
@@ -1,5 +1,6 @@
 use std::iter::once;
 use advtools::prelude::Itertools;
+use advtools::rayon::prelude::*;
 use advtools::input::to_u32;
 
 const INPUT: &str = "136760-595730";
@@ -11,7 +12,7 @@ fn match1(s: &String) -> bool {
         s.chars().tuple_windows().any(|(c1, c2)| c1 == c2)
 }
 
-fn match2(s: &String) -> bool {
+fn match2(s: &&String) -> bool {
     // Test if in any group of 4 digits, only the two middle ones match.
     // To avoid special casing the beginning and end, add non-participating
     // characters there.
@@ -23,10 +24,11 @@ fn match2(s: &String) -> bool {
 fn main() {
     let (min, max) = INPUT.split('-').map(to_u32).next_tuple().unwrap();
 
-    // Make two identical iterators over all numbers matching condition 1.
-    let (i1, i2) = (min..=max).map(|x| x.to_string()).filter(match1).tee();
+    // Find all numbers matching the first condition.
+    let first: Vec<_> = (min..=max).into_par_iter().map(|x| x.to_string())
+                                                   .filter(match1).collect();
+    advtools::print("First round", first.len());
 
-    // For solutions, count all numbers, or only those matching condition 2.
-    advtools::print("First round", i1.count());
-    advtools::print("Second round", i2.filter(match2).count());
+    // Now count only those also matching the second condition.
+    advtools::print("Second round", first.iter().filter(match2).count());
 }
