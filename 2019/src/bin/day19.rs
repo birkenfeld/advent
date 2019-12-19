1e7d71c452b5af81775b3a023874a9ad23daa12d
Georg Brandl <georg@python.org>
2019-12-19 14:31:00 +0100

    2019: Day 19: optimize.

--- 2019/src/bin/day19.rs
+++ 2019/src/bin/day19.rs
@@ -8,13 +8,19 @@ fn main() {
     let is_affected = |x, y| Machine::new(&code).with_input(x)
                                                 .with_input(y).next() == Some(1);
 
-    let count = (0..50).cartesian_product(0..50)
-                       .filter(|&(x, y)| is_affected(x, y)).count();
+    // Get an approximate ratio between x and y of the beam.
+    let x1 = (10..15).find(|&x| is_affected(x, 10)).unwrap() - 1;
+    let x2 = (10..15).rev().find(|&x| is_affected(x, 10)).unwrap() + 1;
+
+    let count = (0..50).cartesian_product(0..50).filter(|&(x, y)| {
+        x >= x1*y/10 && x <= x2*y/10 && is_affected(x, y)
+    }).count();
     advtools::print("Affected tiles", count);
 
+    // Find, by binary search, the correct y and x values.
     let mut xs = HashMap::new();
     let y = binary_search(0, 2000, |y| {
-        let x = binary_search(y, 1400*y/1000, |x| is_affected(x, y + 99));
+        let x = binary_search(x1*y/10, x2*y/10, |x| is_affected(x, y + 99));
         xs.insert(y, x);
         is_affected(x + 99, y)
     });
