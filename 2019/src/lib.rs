a6bbfe3e749f31c9c96eab0ef0e09e868df3ab2a
Georg Brandl <georg@python.org>
2019-12-19 11:49:27 +0100

    2019: Restructure intcode machine to avoid cloning the entire memory.

--- 2019/src/lib.rs
+++ 2019/src/lib.rs
@@ -1,27 +1,35 @@
 use std::collections::HashMap;
-
-pub type Mem = HashMap<i64, i64>;
+use std::rc::Rc;
 
 /// The Intcode computer.
 #[derive(Clone)]
 pub struct Machine {
     ip: i64,
     bp: i64,
-    mem: Mem,
+    mem: Rc<Box<[i64]>>,
+    wmem: HashMap<i64, i64>,
     input: Vec<i64>,
 }
 
 impl Machine {
-    pub fn parse(code: &str) -> Mem {
-        code.trim().split(',')
-                   .enumerate()
-                   .map(|(i, v)| (i as i64, v.parse().expect("invalid memory")))
-                   .collect()
+    /// Parse stringified intcode (Send version).
+    pub fn parse_raw(code: &str) -> Box<[i64]> {
+        code.trim().split(',').map(|v| v.parse().expect("invalid memory")).collect()
     }
 
     /// Create a new machine with given memory cells and initial input.
-    pub fn new(mem: &Mem) -> Self {
-        Self { ip: 0, bp: 0, mem: mem.clone(), input: Vec::new() }
+    pub fn new_raw(mem: &[i64]) -> Self {
+        Self { ip: 0, bp: 0, mem: Rc::new(mem.into()), wmem: HashMap::new(), input: Vec::new() }
+    }
+
+    /// Parse stringified intcode.
+    pub fn parse(code: &str) -> Rc<Box<[i64]>> {
+        Rc::new(Self::parse_raw(code))
+    }
+
+    /// Create a new machine with given memory cells and initial input.
+    pub fn new(mem: &Rc<Box<[i64]>>) -> Self {
+        Self { ip: 0, bp: 0, mem: mem.clone(), wmem: HashMap::new(), input: Vec::new() }
     }
 
     /// Add some input to the machine.
@@ -42,52 +50,47 @@ impl Machine {
 
     /// Return contents of given memory cell.
     pub fn mem(&self, index: i64) -> i64 {
-        self.mem.get(&index).copied().unwrap_or(0)
+        self.wmem.get(&index).or_else(|| self.mem.get(index as usize)).copied().unwrap_or(0)
     }
 
     /// Set contents of given memory cell.
     pub fn set_mem(&mut self, index: i64, value: i64) {
-        self.mem.insert(index, value);
+        // We could check if mem already contains the correct value and avoid
+        // populating the wmem hashmap, but it is worse across all benchmarks.
+        self.wmem.insert(index, value);
     }
 
-    fn reg(&mut self, mode: i64) -> &mut i64 {
-        self.ip += 1;
-        if mode == 0 {
-            // Address mode
-            let addr = self.mem[&(self.ip - 1)];
-            self.mem.entry(addr).or_default()
-        } else if mode == 2 {
-            // Relative address mode
-            let addr = &self.bp + &self.mem[&(self.ip - 1)];
-            self.mem.entry(addr).or_default()
-        } else {
-            // Immediate mode
-            self.mem.entry(self.ip - 1).or_default()
-        }
+    fn set_par(&mut self, mode: i64, val: i64) {
+        let arg_addr = self.ip - 1;
+        let addr = match mode {
+            0 => self.mem(arg_addr),
+            2 => self.bp + self.mem(arg_addr),
+            1 => unreachable!("can't output in immediate mode"),
+            _ => unreachable!("invalid memory mode {}", mode)
+        };
+        self.set_mem(addr, val);
     }
 
-    fn reg_imm(&self, mode: i64, off: i64) -> i64 {
-        let arg = self.mem.get(&(self.ip - 1 + off)).unwrap_or(&0);
-        if mode == 0 {
-            self.mem.get(arg).copied().unwrap_or(0)
-        } else if mode == 2 {
-            self.mem.get(&(self.bp + arg)).copied().unwrap_or(0)
-        } else {
-            *arg
-        }
+    fn par(&self, mode: i64, off: i64) -> i64 {
+        let arg_addr = self.ip + off;
+        self.mem(match mode {
+            0 => self.mem(arg_addr),
+            1 => arg_addr,
+            2 => self.bp + self.mem(arg_addr),
+            _ => unreachable!("invalid memory mode {}", mode)
+        })
     }
 
     fn binop<F: Fn(i64, i64) -> i64>(&mut self, m1: i64, m2: i64, m3: i64, op: F) {
-        let vs = self.reg_imm(m1, 1);
-        let vt = self.reg_imm(m2, 2);
-        let val = op(vs, vt);
-        self.ip += 2;
-        *self.reg(m3) = val;
+        self.ip += 3;
+        let vs = self.par(m1, -3);
+        let vt = self.par(m2, -2);
+        self.set_par(m3, op(vs, vt));
     }
 
     fn jumpop<F: Fn(i64) -> bool>(&mut self, m1: i64, m2: i64, cond: F) {
-        if cond(self.reg_imm(m1, 1)) {
-            self.ip = self.reg_imm(m2, 2);
+        if cond(self.par(m1, 0)) {
+            self.ip = self.par(m2, 1);
         } else {
             self.ip += 2;
         }
@@ -101,7 +104,7 @@ impl Iterator for Machine {
 
     fn next(&mut self) -> Option<i64> {
         loop {
-            let opcode = self.mem.get(&self.ip).expect("no more code?");
+            let opcode = self.mem(self.ip);
             self.ip += 1;
             let m3 = opcode / 10000;
             let m2 = (opcode / 1000) % 10;
@@ -110,13 +113,13 @@ impl Iterator for Machine {
             match op {
                 1 => self.binop(m1, m2, m3, |a, b| a + b),
                 2 => self.binop(m1, m2, m3, |a, b| a * b),
-                3 => *self.reg(m1) = self.input.remove(0),
-                4 => return Some(*self.reg(m1)),
+                3 => { self.ip += 1; let v = self.input.remove(0); self.set_par(m1, v); }
+                4 => { self.ip += 1; return Some(self.par(m1, -1)); }
                 5 => self.jumpop(m1, m2, |a| a != 0),
                 6 => self.jumpop(m1, m2, |a| a == 0),
                 7 => self.binop(m1, m2, m3, |a, b| (a < b) as i64),
                 8 => self.binop(m1, m2, m3, |a, b| (a == b) as i64),
-                9 => { let rel = *self.reg(m1); self.bp += rel; },
+                9 => { self.ip += 1; self.bp += self.par(m1, -1); },
                 99 => return None,
                 d => panic!("unknown opcode {}", d)
             }
