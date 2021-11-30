dded0ee128c86a8cca002021d340bf371b51410c
Georg Brandl <georg@python.org>
2021-11-30 08:56:17 +0100

    2021: Lets go 2021.

---
+++ 2021/src/bin/day01.rs
@@ -0,0 +1,9 @@
+use advtools::itertools::Itertools;
+use advtools::input::iter_input;
+
+fn main() {
+    let input = iter_input();
+
+    advtools::verify("", 0, 0);
+    advtools::verify("", 0, 0);
+}
