fe15e8309ef6a2f7c9540e1ce5880809ae6a9748
Georg Brandl <georg@python.org>
2021-12-19 16:48:57 +0100

    2021: Day 19: refactor a bit, add comments.

--- 2021/src/bin/day19.rs
+++ 2021/src/bin/day19.rs
@@ -25,7 +25,7 @@ impl std::ops::Sub<Vector> for Vector {
     }
 }
 
-const FORMAT: &str = r"(-?\d+),(-?\d+),(-?\d+)|(.*)";
+const FORMAT: &str = r"([-\d]+),([-\d]+),([-\d]+)|(.*)";
 const ORIENT: &[fn(Vector) -> Vector] = &[
     // Two exchanges, two negations
     |Vector(x, y, z)| Vector(x, y, z),
@@ -56,54 +56,60 @@ const ORIENT: &[fn(Vector) -> Vector] = &[
 ];
 
 fn main() {
-    let mut scanner_beacons: Vec<Vec<_>> = vec![];
+    // Parse the input.
+    let mut beacons: Vec<Vec<_>> = vec![];
     for line in input::rx_lines::<Option<(i32, i32, i32)>>(FORMAT) {
         if let Some(c) = line {
-            scanner_beacons.last_mut().unwrap().push(Vector(c.0, c.1, c.2));
+            beacons.last_mut().unwrap().push(Vector(c.0, c.1, c.2));
         } else {
-            scanner_beacons.push(vec![]);
+            beacons.push(vec![]);  // start of a new scanner
         }
     }
-    let mut orients = HashMap::new();
-    let mut positions = HashMap::new();
-    orients.insert(0, ORIENT[0]);
-    positions.insert(0, Vector(0, 0, 0));
+    // Define scanner 0 to be at the origin with known orientation.
+    let mut positions: HashMap<_, _> = [(0, Vector(0, 0, 0))].into_iter().collect();
+    let mut all_beacons: HashSet<_> = beacons[0].iter().cloned().collect();
 
-    let mut todo: HashSet<_> = (1..scanner_beacons.len()).collect();
-
+    // Loop until all scanner locations and orientations are known.
     'outer:
-    while !todo.is_empty() {
-        for &j in &todo {
-            for (&i, i_orient) in &orients {
-                if let Some((j_orient, rel_vec)) = ORIENT.par_iter().find_map_any(|&j_orient| {
-                    let mut counts = HashMap::<_, u8>::new();
-                    iproduct!(&scanner_beacons[i], &scanner_beacons[j])
-                        .map(|(&bi, &bj)| i_orient(bi) - j_orient(bj))
+    while positions.len() < beacons.len() {
+        for j in (0..beacons.len()).filter(|j| !positions.contains_key(j)) {
+            // For each unoriented scanner j, go through the known scanners i
+            // and try to find an overlap.
+            for &i in positions.keys() {
+                // Go through all possible orientations of scanner j in parallel.
+                if let Some((orient, pos)) = ORIENT.par_iter().find_map_any(|&orient| {
+                    let mut counts = HashMap::<_, u32>::new();
+                    // For each pair of beacons, get the relative vector between
+                    // the two.  If the scanner ranges overlap, at least 12
+                    // beacon pairs will have the same relative vector, which will
+                    // also be the position of scanner j (since all known scanners'
+                    // beacon coordinates were made relative to the origin).
+                    iproduct!(&beacons[i], &beacons[j])
+                        .map(|(&bi, &bj)| bi - orient(bj))
                         .find_map(|rel_vec| {
                             let count = counts.entry(rel_vec).or_default();
                             *count += 1;
-                            if *count == 12 { Some((j_orient, rel_vec)) } else { None }
+                            if *count == 12 { Some((orient, rel_vec)) } else { None }
                         })
                 }) {
-                    todo.take(&j);
-                    orients.insert(j, j_orient);
-                    positions.insert(j, positions[&i] + rel_vec);
+                    // Found an overlap: convert this scanners' beacon coords to
+                    // scanner 0's coordinate system, record its position, and
+                    // then start again with the rest.
+                    beacons[j].iter_mut().for_each(|p| *p = pos + orient(*p));
+                    all_beacons.extend(beacons[j].iter().cloned());
+                    positions.insert(j, pos);
                     continue 'outer;
                 }
             }
         }
     }
 
-    let mut beacons = HashSet::new();
-    for (i, coords) in scanner_beacons.iter().enumerate() {
-        let pos = positions[&i];
-        let orient = orients[&i];
-        for &coord in coords {
-            beacons.insert(pos + orient(coord));
-        }
-    }
-    advtools::verify("Number of beacons", beacons.len(), 419);
+    // Part 1: count unique coordinates of all beacons.
+    advtools::verify("Number of beacons", all_beacons.len(), 419);
 
-    let max_dist = positions.values().tuple_combinations().map(|(&p1, &p2)| (p1 - p2).dist()).max().unwrap();
+    // Part 2: Go over all scanner combinations to get the maximum Manhattan distance.
+    let max_dist = positions.values().tuple_combinations()
+                                     .map(|(&p1, &p2)| (p1 - p2).dist())
+                                     .max().unwrap();
     advtools::verify("Max distance", max_dist, 13210);
 }
