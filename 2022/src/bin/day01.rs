cb12812a07d72fabfe3337e3040ca742cbb296eb
Georg Brandl <georg@python.org>
2022-12-03 12:32:29 +0100

    2022: Day 1: slight improvement.

--- 2022/src/bin/day01.rs
+++ 2022/src/bin/day01.rs
@@ -5,11 +5,12 @@ fn main() {
     // Split input by empty lines.
     let chunks = input::string().split("\n\n");
 
-    // Sum all the calories for a single elf, and sort the sums (descending).
-    let all_cals = chunks.map(|elf| {
-        elf.split('\n').map(|item| item.parse::<i32>().unwrap()).sum::<i32>()
-    }).sorted_by_key(|k| -k).collect_vec();
+    // Sum all the calories for a single elf, sort the sums descending, and
+    // get the highest three.
+    let (c1, c2, c3) = chunks.map(|elf| {
+        elf.lines().flat_map(str::parse::<i32>).sum::<i32>()
+    }).sorted_by_key(|k| -k).next_tuple().unwrap();
 
-    advtools::verify("1-elf maximum", all_cals[0], 70698);
-    advtools::verify("3-elf maximum", all_cals[0..3].iter().sum::<i32>(), 206643);
+    advtools::verify("1-elf maximum", c1, 70698);
+    advtools::verify("3-elf maximum", c1 + c2 + c3, 206643);
 }
