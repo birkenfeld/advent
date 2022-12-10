3c42e95796c3cdb8d75b5ce4d20e9184b1a2aaa9
Georg Brandl <georg@python.org>
2022-12-10 13:06:33 +0100

    2022: Day 6: use position().

--- 2022/src/bin/day06.rs
+++ 2022/src/bin/day06.rs
@@ -4,10 +4,9 @@ use advtools::prelude::Itertools;
 fn main() {
     let input = input::string().as_bytes();
 
-    let find = |n| input.windows(n)
-                        .enumerate()
-                        .find(|(_, win)| win.iter().all_unique())
-                        .map(|(i, _)| i + n).unwrap();
+    let find = |n| n + input.windows(n)
+                            .position(|win| win.iter().all_unique())
+                            .unwrap();
 
     advtools::verify("Start of packet", find(4), 1779);
     advtools::verify("Start of message", find(14), 2635);
