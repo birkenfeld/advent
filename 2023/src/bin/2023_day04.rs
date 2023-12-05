f5f60f27bfb0642f9ef52b0a61114741620195a0
Georg Brandl <georg@python.org>
2023-12-05 09:54:25 +0100

    Day 4: no need for parsing

--- 2023/src/bin/2023_day04.rs
+++ 2023/src/bin/2023_day04.rs
@@ -2,20 +2,17 @@ use advtools::input;
 use advtools::prelude::{HashSet, HashMap};
 
 fn main() {
-    let mut winning = 0;
+    let mut points = 0;
     let mut instances = HashMap::<usize, usize>::new();
 
     for (i, (num1, num2)) in input::rx_lines::<(&str, &str)>(r".*: (.*) \| (.*)").enumerate() {
-        // Parse the numbers and determine the number of matches.
-        let winners = num1.split_whitespace().map(|n| n.parse::<u32>().unwrap())
-                                             .collect::<HashSet<_>>();
-        let commons = num2.split_whitespace().map(|n| n.parse::<u32>().unwrap())
-                                             .filter(|n| winners.contains(n))
-                                             .count();
+        // Extract the numbers and determine the number of matches.
+        let winners = num1.split_whitespace().collect::<HashSet<_>>();
+        let commons = num2.split_whitespace().filter(|n| winners.contains(n)).count();
 
         // Part 1: add up power-of-two points for winning numbers.
         if commons > 0 {
-            winning += 1 << (commons - 1);
+            points += 1 << (commons - 1);
         }
 
         // Part 2: keep track of card amounts.
@@ -25,6 +22,6 @@ fn main() {
         }
     }
 
-    advtools::verify("Points total", winning, 20855);
+    advtools::verify("Points total", points, 20855);
     advtools::verify("Final cards", instances.values().sum::<usize>(), 5489600);
 }
