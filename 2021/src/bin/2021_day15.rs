99371a4a08eccd80ffc6a08dbb710197447bc246
Georg Brandl <georg@python.org>
2023-12-17 20:53:18 +0100

    2021 day 15: use Dijkstra.

--- 2021/src/bin/2021_day15.rs
+++ 2021/src/bin/2021_day15.rs
@@ -1,34 +1,34 @@
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
 use advtools::prelude::{Itertools, HashMap};
 use advtools::input;
 use advtools::grid::{Grid, Pos};
 
-// BFS search through the grid for the minimum-cost way to reach the exit
+// Dijkstra search through the grid for the minimum-cost way to reach the exit
 // (bottom right) position.
 fn visit(grid: &Grid<u8>) -> u32 {
+    let goal = Pos(grid.width() as u16 - 1, grid.height() as u16 - 1);
     // All the positions we have discovered, mapped to the minimal cost of
     // reaching them.
     let mut visited = HashMap::with_capacity(10000);
-    // All the current positions we need to check, and the risk so far
-    // to reach them.
-    let mut queue = HashMap::new();
-    queue.insert(Pos(0u16, 0u16), 0);
+    // All the current positions we need to check, sorted by the risk so far to
+    // reach them.
+    let mut queue = BinaryHeap::new();
+    queue.push((Reverse(0), Pos(0u16, 0u16)));
 
     loop {
-        for (pos, risk) in std::mem::replace(&mut queue, HashMap::with_capacity(10000)) {
-            // Check each neighbor for new positions, or positions with a
-            // lower-risk path to them.
-            for nbpos in grid.neighbors(pos) {
-                let new_risk = risk + grid[nbpos] as u32;
-                if visited.get(&nbpos).map_or(true, |&old_risk| old_risk > new_risk) {
-                    visited.insert(nbpos, new_risk);
-                    queue.insert(nbpos, new_risk);
-                }
-            }
+        let (Reverse(risk), pos) = queue.pop().unwrap();
+        if pos == goal {
+            return risk;
         }
-        if queue.is_empty() {
-            // Nothing new to check -> we must have found the best way.
-            let goal = Pos(grid.width() as u16 - 1, grid.height() as u16 - 1);
-            return visited[&goal];
+        // Check each neighbor for new positions, or positions with a lower-risk
+        // path to them.
+        for nbpos in grid.neighbors(pos) {
+            let new_risk = risk + grid[nbpos] as u32;
+            if visited.get(&nbpos).map_or(true, |&old_risk| old_risk > new_risk) {
+                visited.insert(nbpos, new_risk);
+                queue.push((Reverse(new_risk), nbpos));
+            }
         }
     }
 }
