278532a6de331fa574f1cf564d8177642ca41dca
Georg Brandl <georg@python.org>
2023-12-17 11:23:55 +0100

    Optimize 2023 day 10 with ray casting.

--- 2023/src/bin/2023_day10.rs
+++ 2023/src/bin/2023_day10.rs
@@ -2,37 +2,20 @@ use advtools::input;
 use advtools::grid::{Grid, Dir, Dir::*, Pos};
 use advtools::prelude::Itertools;
 
-#[derive(Clone, Copy, PartialEq, Eq)]
-enum Tile {  // for part 2
-    Pipe,
-    Unmarked,
-    Outside
-}
-
-fn flood_fill(grid: &mut Grid<Tile>, pos: Pos<u32>) {
-    grid[pos] = Tile::Outside;
-    grid.neighbors(pos).for_each(
-        |nbpos| if grid[nbpos] == Tile::Unmarked {
-            flood_fill(grid, nbpos)
-        }
-    )
-}
-
 fn main() {
     // Parse the input.
     let mut grid = Grid::new(input::lines().map(|line| line.chars().map(|ch| match ch {
         '|' => [U, D],
         '-' => [L, R],
-        'J' => [L, U],
-        'L' => [R, U],
-        '7' => [L, D],
-        'F' => [R, D],
+        'J' => [U, L],
+        'L' => [U, R],
+        '7' => [D, L],
+        'F' => [D, R],
         'S' => [D, D], // later
         _   => [U, U], // dummy
     })));
-    // Keep a new grid for the second part, with double the size, so that
-    // we can easily flood-fill "squeezing" between the pipes.
-    let mut grid2 = Grid::fill(Tile::Unmarked, grid.width()*2 + 1, grid.height()*2 + 1);
+    // Keep a new grid for the second part.
+    let mut pipe = Grid::fill(None, grid.width(), grid.height());
 
     // Find start position and reconstruct its pipe.
     let start = grid.find_pos(|&tile| tile == [D, D]).unwrap();
@@ -52,11 +35,8 @@ fn main() {
                 pos = pos.to(dir);
                 steps += 1;
 
-                // Mark the pipe in grid2.
-                let grid2pos = Pos(pos.x*2 + 1, pos.y*2 + 1);
-                grid2[grid2pos] = Tile::Pipe;
-                grid2[grid2pos + grid[pos][0]] = Tile::Pipe;
-                grid2[grid2pos + grid[pos][1]] = Tile::Pipe;
+                // Mark pieces of our pipe loop.
+                pipe[pos] = Some(grid[pos]);
 
                 if pos == start {
                     break 'outer;
@@ -67,10 +47,18 @@ fn main() {
     }
     advtools::verify("Steps to furthest point", steps / 2, 7102);
 
-    // Part 2: Find enclosed tiles.
-    flood_fill(&mut grid2, Pos(0, 0));
-    let enclosed = grid2.positions().filter(|pos: &Pos<u32>| {
-        grid2[*pos] == Tile::Unmarked && pos.x % 2 == 1 && pos.y % 2 == 1
-    }).count();
+    // Part 2: Raycast through grid2 for enclosed tiles.
+    let mut enclosed = 0;
+    for y in 0..grid.height() {
+        let mut inside = false;
+        for x in 0..grid.width() {
+            match pipe[Pos(x, y)] {
+                // Need to ignore tiles that don't change the "in/out" state.
+                Some([L, R] | [D, R|L]) => (),
+                Some(_) => inside = !inside,
+                _ => enclosed += inside as usize
+            }
+        }
+    }
     advtools::verify("Enclosed tiles", enclosed, 363);
 }
