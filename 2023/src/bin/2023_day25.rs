52ebcc58002b30c93af0d896bf2a88fe98c045c8
Georg Brandl <g.brandl@fz-juelich.de>
2024-11-22 10:43:42 +0100

    2023 day 25.

--- 2023/src/bin/2023_day25.rs
+++ 2023/src/bin/2023_day25.rs
@@ -1,24 +1,23 @@
 use advtools::input;
-use advtools::prelude::HashSet;
+use advtools::prelude::HashMap;
+use rustworkx_core::petgraph::graph::UnGraph;
+use rustworkx_core::connectivity::stoer_wagner_min_cut;
 
 fn main() {
-    input::set("jqt: rhn xhk nvd
-rsh: frs pzl lsr
-xhk: hfx
-cmg: qnr nvd lhk bvb
-rhn: xhk bvb hfx
-bvb: xhk hfx
-pzl: lsr hfx nvd
-qnr: nvd
-ntq: jqt hfx bvb xhk
-nvd: lhk
-lsr: lhk
-rzs: qnr cmg lsr rsh
-frs: qnr lhk lsr");
+    let mut graph = UnGraph::new_undirected();
+    let mut nodes = HashMap::new();
 
     for line in input::rx_lines(r"(\w+): (.+)") {
-        let (name, deps): (&str, &str) = line;
-        let deps = deps.split(' ').collect::<HashSet<_>>();
-        println!("{}: {:?}", name, deps);
+        let (first, others): (&str, &str) = line;
+        let first = *nodes.entry(first).or_insert_with(|| graph.add_node(()));
+        for other in others.split(' ') {
+            let other = *nodes.entry(other).or_insert_with(|| graph.add_node(()));
+            graph.add_edge(first, other, ());
+        }
     }
+
+    let (n, cut) = stoer_wagner_min_cut(&graph, |_| Ok::<_, ()>(1)).unwrap().unwrap();
+    assert_eq!(n, 3);
+
+    advtools::verify("Product of remains", cut.len() * (nodes.len() - cut.len()), 600225);
 }
