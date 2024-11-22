28aedbd9f42b4f08a12151c0ac0bee2d77b8f859
Georg Brandl <g.brandl@fz-juelich.de>
2024-11-22 10:12:15 +0100

    wip

--- 2023/src/bin/2023_day21.rs
+++ 2023/src/bin/2023_day21.rs
@@ -2,18 +2,20 @@ use advtools::input;
 use advtools::grid::{Grid, Pos};
 use advtools::prelude::{Itertools, HashSet};
 
-fn parity(pos: Pos<usize>) -> bool {
-    (pos.x + pos.y) % 2 == 1
+fn parity(pos: Pos<i32>) -> bool {
+    (pos.x + pos.y).rem_euclid(2) == 0
 }
 
-fn visit(maze: &Grid<bool>, start: Pos<usize>, steps: usize) -> usize {
+fn visit(maze: &Grid<bool>, start: Pos<i32>, steps: i64) -> i64 {
+    let (w, h) = (maze.width() as i32, maze.height() as i32);
     let mut seen = HashSet::new();
     seen.insert(start);
     let mut queue = vec![start];
     for _ in 1..steps+1 {
         for pos in std::mem::take(&mut queue) {
             for nbpos in pos.neighbors() {
-                if !maze[nbpos] {
+                // Taking the modulo here allows the infinite maze of part 2.
+                if !maze[Pos(nbpos.x.rem_euclid(w), nbpos.y.rem_euclid(h))] {
                     continue;
                 }
                 if !seen.insert(nbpos) {
@@ -23,36 +25,47 @@ fn visit(maze: &Grid<bool>, start: Pos<usize>, steps: usize) -> usize {
             }
         }
     }
-    println!("{}", seen.len());
-    seen.into_iter().filter(|&pos| parity(pos) == parity(start)).count()
+    seen.into_iter()
+        .filter(|&pos| parity(pos) == parity(start) ^ (steps % 2 != 0))
+        .count() as _
 }
 
 fn main() {
-//     input::set("\
-// ...........
-// .....###.#.
-// .###.##..#.
-// ..#.#...#..
-// ....#.#....
-// .##..S####.
-// .##..#...#.
-// .......##..
-// .##.#.####.
-// .##..##.##.
-// ...........
-// ");
     let mut start = Pos(0, 0);
     let grid = Grid::new(input::lines().enumerate().map(|(y, line)| {
         line.chars().enumerate().map(|(x, ch)| match ch {
             '.' => true,
             '#' => false,
             'S' => {
-                start = Pos(x, y);
+                start = Pos(x as i32, y as i32);
                 true
             }
             _ => unreachable!()
         }).collect_vec()
     }));
 
-    advtools::verify("Reachable positions", visit(&grid, start, 64), 3762);
+    // Part 1: just visit once.
+    const STEPS_1: i64 = 64;
+    advtools::verify("Reachable positions", visit(&grid, start, STEPS_1), 3762);
+
+    // Part 2: assume the number of reachable positions grows regularly in
+    // quadratic fashion, so we can sample at three points.
+    const STEPS_2: i64 = 26501365;
+    let w = grid.width() as i64;
+    let x = (STEPS_2 - w/2) / w;
+
+    let mut y = Vec::new();
+    for i in 0..3 {
+        let steps = w/2 + i*w;
+        y.push(visit(&grid, start, steps));
+    }
+
+    // Calculate the coefficients of the quadratic function.
+    let a = (y[2] - 2*y[1] + y[0]) / 2;
+    let b = y[1] - y[0] - a;
+    let c = y[0];
+
+    let result = a*x*x + b*x + c;
+
+    advtools::verify("Reachable for many steps", result, 621944727930768_usize);
 }
--- 2023/src/bin/2023_day25.rs
+++ 2023/src/bin/2023_day25.rs
@@ -1,79 +1,24 @@
 use advtools::input;
-use advtools::grid::{Grid, Pos, Dir};
 use advtools::prelude::HashSet;
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
-enum Tile {
-    Forest,
-    Path,
-    Slope(Dir),
-}
-
-fn dfs(grid: &Grid<Tile>, pos: Pos, target: Pos, steps: usize,
-       longest: &mut usize, seen: &mut HashSet<Pos>, consider_slopes: bool) {
-    seen.insert(pos);
-    for nbpos in grid.neighbors(pos) {
-        if seen.contains(&nbpos) {
-            continue;
-        }
-        match grid[nbpos] {
-            Tile::Forest => continue,
-            Tile::Path => {
-                if nbpos == target {
-                    *longest = (*longest).max(steps + 1);
-                    println!("{}", longest);
-                } else {
-                    dfs(grid, nbpos, target, steps + 1, longest, seen, consider_slopes);
-                }
-            }
-            Tile::Slope(slopedir) => {
-                if !consider_slopes || slopedir == nbpos.dir_from(pos) {
-                    dfs(grid, nbpos, target, steps + 1, longest, seen, consider_slopes);
-                }
-            }
-        }
-    }
-    seen.remove(&pos);
-}
-
 fn main() {
-//     input::set("\
-// #.#####################
-// #.......#########...###
-// #######.#########.#.###
-// ###.....#.>.>.###.#.###
-// ###v#####.#v#.###.#.###
-// ###.>...#.#.#.....#...#
-// ###v###.#.#.#########.#
-// ###...#.#.#.......#...#
-// #####.#.#.#######.#.###
-// #.....#.#.#.......#...#
-// #.#####.#.#.#########v#
-// #.#...#...#...###...>.#
-// #.#.#v#######v###.###v#
-// #...#.>.#...>.>.#.###.#
-// #####v#.#.###v#.#.###.#
-// #.....#...#...#.#.#...#
-// #.#########.###.#.#.###
-// #...###...#...#...#.###
-// ###.###.#.###v#####v###
-// #...#...#.#.>.>.#.>.###
-// #.###.###.#.###.#.#v###
-// #.....###...###...#...#
-// #####################.#
-// ");
-    let grid = Grid::new(input::lines().map(|line| line.chars().map(|ch| match ch {
-        '#' => Tile::Forest,
-        '.' => Tile::Path,
-        sl  => Tile::Slope(Dir::from_char(sl)),
-    })));
-    let start = Pos(1, 0);
-    let target = Pos(grid.width() as i32 - 2, grid.height() as i32 - 1);
+    input::set("jqt: rhn xhk nvd
+rsh: frs pzl lsr
+xhk: hfx
+cmg: qnr nvd lhk bvb
+rhn: xhk bvb hfx
+bvb: xhk hfx
+pzl: lsr hfx nvd
+qnr: nvd
+ntq: jqt hfx bvb xhk
+nvd: lhk
+lsr: lhk
+rzs: qnr cmg lsr rsh
+frs: qnr lhk lsr");
 
-    let mut longest = 0;
-    dfs(&grid, start, target, 0, &mut longest, &mut HashSet::new(), true);
-    advtools::verify("Longest hike", longest, 2402);
-
-    dfs(&grid, start, target, 0, &mut longest, &mut HashSet::new(), false);
-    advtools::verify("Longest hike without slopes", longest, 6450);
+    for line in input::rx_lines(r"(\w+): (.+)") {
+        let (name, deps): (&str, &str) = line;
+        let deps = deps.split(' ').collect::<HashSet<_>>();
+        println!("{}: {:?}", name, deps);
+    }
 }
